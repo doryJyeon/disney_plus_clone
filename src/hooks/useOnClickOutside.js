0e50ef048bce71860e03c576f93561f46e29ac60
JYeon <doryJYeon@gmail.com>
2024-03-01 12:33:07 +0900

    [+] modal outside click handler

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -6,10 +6,11 @@ export default function useOnClickOutside (ref, handler) {
       if(!ref.current || ref.current.contains(event.target)) {
         return false
       }
+      handler()
     }
+    
     document.addEventListener("mousedown", listener)
     document.addEventListener("touchstart", listener)
-
     return () => {
       document.removeEventListener("mousedown", listener)
       document.removeEventListener("touchstart", listener)
