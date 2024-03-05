672245dc9af73400fa743056a6396225d478b318
JYeon <doryJYeon@gmail.com>
2024-03-05 21:26:40 +0900

    [+] detail css

--- src/pages/DetailPage/index.js
+++ src/pages/DetailPage/index.js
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from '../../api/axios'
+import "./DetailPage.css"
 
 const DetailPage = () => {
   let { movieId } = useParams()
@@ -21,6 +22,10 @@ const DetailPage = () => {
   return (
     <section>
       <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="img" className="modal__poster-img" />
+      <p className="movie__date">{movie.release_date} | {movie.runtime} min</p>
+      <p className="movie__name">{movie.title? movie.title : movie.name}</p>
+      <p className="movie__tagline">{movie.tagline}</p>
+      <p className="movie__description">{movie.overview}</p>
     </section>
   )
 }
